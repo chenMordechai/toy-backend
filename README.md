2cdde15f8bb018f99aa8853d958c68cc405a5b09
Chen <chen100030@gmail.com>
2023-11-30 23:10:42 +0200

    new server

---
+++ README.md
@@ -0,0 +1,10 @@
+A powerful backend with:
+*. Auth and REST
+*. Route splitting
+*. Middlewares
+*. Logger
+*. Config
+*. MongoDB filtering
+*. MongoDB ObjectId
+*. ( _id vs id ) - Pushing data into an array in a MongoDB document
+*. Vanilla frontend as a driver for testing    
--- server.js
+++ server.js
@@ -1,204 +1,59 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
 
-import { toyService } from './services/toy.service.js'
-import { userService } from './services/user.service.js'
-import { loggerService } from './services/logger.service.js';
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import { logger } from './services/logger.service.js'
+logger.info('server.js loaded...')
+
 const app = express()
 
-
-// App Configuration
-const corsOptions = {
-    origin: [
-        'http://127.0.0.1:8080',
-        'http://localhost:8080',
-        'http://127.0.0.1:5173',
-        'http://localhost:5173',
-    ],
-    credentials: true
-}
-
-app.use(cors(corsOptions))
+// Express App Config
 app.use(cookieParser()) // for res.cookies
 app.use(express.json()) // for req.body
 app.use(express.static('public'))
 
 
-
-// **************** Toys API ****************:
-// List
-app.get('/api/toy', (req, res) => {
-    const { name, price, inStock, labels, type, desc } = req.query
-    const filterBy = { name, price: +price, inStock, labels: (labels) ? labels : [] }
-    const sortBy = { type, desc: +desc }
-    toyService.query(filterBy, sortBy)
-        .then(toys => {
-            res.send(toys)
-        })
-        .catch(err => {
-            loggerService.error('Cannot load toys', err)
-            res.status(400).send('Cannot load toys')
-        })
-})
-
-// Add
-app.post('/api/toy', (req, res) => {
-    // const loggedinUser = userService.validateToken(req.cookies.loginToken)
-    // if (!loggedinUser) return res.status(401).send('Cannot add toy')
-    const { name, price, labels, inStock } = req.body
-    console.log(name, price, labels, inStock)
-
-    const toy = {
-        name,
-        price: +price,
-        labels,
-        inStock
+if (process.env.NODE_ENV === 'production') {
+    // Express serve static files on production environment
+    app.use(express.static(path.resolve(__dirname, 'public')))
+    console.log('__dirname: ', __dirname)
+} else {
+    // Configuring CORS
+    const corsOptions = {
+        // Make sure origin contains the url your frontend is running on
+        origin: ['http://127.0.0.1:5173', 'http://localhost:5173', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+        credentials: true
     }
-    // toyService.save(toy, loggedinUser)
-    toyService.save(toy)
-        .then(savedToy => {
-            res.send(savedToy)
-        })
-        .catch(err => {
-            loggerService.error('Cannot add toy', err)
-            res.status(400).send('Cannot add toy')
-        })
+    app.use(cors(corsOptions))
+}
+
+import { authRoutes } from './api/auth/auth.routes.js'
+import { userRoutes } from './api/user/user.routes.js'
+import { toyRoutes } from './api/toy/toy.routes.js'
+
+// routes
+app.use('/api/auth', authRoutes)
+app.use('/api/user', userRoutes)
+app.use('/api/toy', toyRoutes)
+
+// Make every unmatched server-side-route fall back to index.html
+// So when requesting http://localhost:3030/index.html/car/123 it will still respond with
+// our SPA (single page app) (the index.html file) and allow vue-router to take it from there
+
+
+app.get('/**', (req, res) => {
+    res.sendFile(path.resolve('public/index.html'))
 })
 
-// Edit
-app.put('/api/toy', (req, res) => {
-    // const loggedinUser = userService.validateToken(req.cookies.loginToken)
-    // if (!loggedinUser) return res.status(401).send('Cannot update toy')
-
-    const { name, price, labels, inStock, _id } = req.body
-    console.log(name, price, labels, inStock)
-
-    const toy = {
-        name,
-        price: +price,
-        labels,
-        inStock,
-        _id
-    }
-    // toyService.save(toy, loggedinUser)
-    toyService.save(toy)
-        .then((savedToy) => {
-            res.send(savedToy)
-        })
-        .catch(err => {
-            loggerService.error('Cannot update toy', err)
-            res.status(400).send('Cannot update toy')
-        })
-
-})
-
-// Read - getById
-app.get('/api/toy/:toyId', (req, res) => {
-    const { toyId } = req.params
-    toyService.get(toyId)
-        .then(toy => {
-            // toy.msgs =['HEllo']
-            res.send(toy)
-        })
-        .catch(err => {
-            loggerService.error('Cannot get toy', err)
-            res.status(400).send(err)
-        })
-})
-
-// Remove
-app.delete('/api/toy/:toyId', (req, res) => {
-    // const loggedinUser = userService.validateToken(req.cookies.loginToken)
-    // if (!loggedinUser) return res.status(401).send('Cannot delete toy')
-
-    const { toyId } = req.params
-    // toyService.remove(toyId, loggedinUser)
-    toyService.remove(toyId)
-        .then(msg => {
-            res.send({ msg, toyId })
-        })
-        .catch(err => {
-            loggerService.error('Cannot delete toy', err)
-            res.status(400).send('Cannot delete toy, ' + err)
-        })
-})
-
-
-// **************** Users API ****************:
-app.get('/api/auth/:userId', (req, res) => {
-    const { userId } = req.params
-    userService.getById(userId)
-        .then(user => {
-            res.send(user)
-        })
-        .catch(err => {
-            loggerService.error('Cannot get user', err)
-            res.status(400).send('Cannot get user')
-        })
-})
-
-app.post('/api/auth/login', (req, res) => {
-    const credentials = req.body
-    userService.checkLogin(credentials)
-        .then(user => {
-            const token = userService.getLoginToken(user)
-            res.cookie('loginToken', token)
-            res.send(user)
-        })
-        .catch(err => {
-            loggerService.error('Cannot login', err)
-            res.status(401).send('Not you!')
-        })
-})
-
-app.post('/api/auth/signup', (req, res) => {
-    const credentials = req.body
-    userService.save(credentials)
-        .then(user => {
-            const token = userService.getLoginToken(user)
-            res.cookie('loginToken', token)
-            res.send(user)
-        })
-        .catch(err => {
-            loggerService.error('Cannot signup', err)
-            res.status(401).send('Nope!')
-        })
-})
-
-
-app.post('/api/auth/logout', (req, res) => {
-    res.clearCookie('loginToken')
-    res.send('logged-out!')
-})
-
-
-app.put('/api/user', (req, res) => {
-    const loggedinUser = userService.validateToken(req.cookies.loginToken)
-    if (!loggedinUser) return res.status(401).send('No logged in user')
-    const { diff } = req.body
-    if (loggedinUser.score + diff < 0) return res.status(400).send('No credit')
-    loggedinUser.score += diff
-    return userService.save(loggedinUser).then(user => {
-        const token = userService.getLoginToken(user)
-        res.cookie('loginToken', token)
-        res.send(user)
-    })
-})
-
-
-// app.get('/**', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
-
-
-
-// Listen will always be the last line in our server!
 const port = process.env.PORT || 3030
+
 app.listen(port, () => {
-    loggerService.info(`Server listening on port http://127.0.0.1:${port}/`)
+    logger.info('Server is running on port: ' + port)
 })
+
+
